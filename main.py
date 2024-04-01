26c8e2c41fde27ae2d5ca7ff576fef4b3aae4b85
ibrohimovMirzobek <aviation.3704@gmail.com>
2024-04-01 16:59:00 +0500

    ok

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-
+# Import main library
 import pygame
-
+# Pygame initialize
 pygame.init()
 
+# Main window parameter
 screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
 icon = pygame.image.load('images/tank1.png')
 pygame.display.set_caption("TANK1990")
 
+# Pygame Clock
+clock = pygame.time.Clock()
+bullet_image = pygame.Surface((4, 4))
+
 # Import image
 imgBrick = pygame.image.load('images/block_brick.png')
-imgTanks = [
+
+# Import player tank image
+image_tank_list = [
     pygame.image.load('images/tank1.png'),
     pygame.image.load('images/tank2.png'),
     pygame.image.load('images/tank3.png'),
     pygame.image.load('images/tank4.png'),
-    pygame.image.load('images/tank5.png'),
-    pygame.image.load('images/tank6.png'),
-    pygame.image.load('images/tank7.png'),
-    pygame.image.load('images/tank8.png'),
     ]
-imgBangs = [
+
+# Import enemy tank image
+image_enemy_tank_list =[
+    pygame.image.load('images/enemy_tank_100.png'),
+    pygame.image.load('images/enemy_tank_200.png'),
+    pygame.image.load('images/enemy_tank_300.png'),
+    pygame.image.load('images/enemy_tank_400.png'),
+    ]
+
+#Import explosion effect image
+image_explosion_list = [
     pygame.image.load('images/bang1.png'),
     pygame.image.load('images/bang2.png'),
     pygame.image.load('images/bang3.png'),
     ]
-imgBonuses = [
+
+# Import image for bonus
+image_bonuses_list = [
     pygame.image.load('images/bonus_star.png'),
     pygame.image.load('images/bonus_tank.png'),
     ]
@@ -46,43 +60,51 @@ sounds = [
           ]
 
 # tank start coordinate
-tank_coord_x = 100
-tank_coord_y = 100
+tank_coordinate_x = 100
+tank_coordinate_y = 100
 tank_speed = 1
 
 # bullet start coordinate
 bullets = []
-bullet_coord_x = tank_coord_x
-bullet_coord_y = tank_coord_yJS
-gameplay = True
-while gameplay:
+bullet_coord_x = tank_coordinate_x
+bullet_coord_y = tank_coordinate_y
 
+gameplay = True
+
+# Main loop gameplay
+while gameplay:
+    pygame.display.flip()
+    clock.tick(60)
+    screen.fill('Black')
     pygame.draw.rect(screen, (100, 100, 100), (0, 0, 100, 600,))
     pygame.draw.rect(screen, (100, 100, 100), (700, 0, 100, 600,))
-    im = pygame.transform.rotate(imgTanks[1], turn_1)
-    screen.blit(im, (tank_coord_x, tank_coord_y))
+    screen.blit(bullet_image, (tank_coordinate_x + 15, tank_coordinate_y + 15))
+    im = pygame.transform.rotate(image_tank_list[0], turn_1)
+    screen.blit(im, (tank_coordinate_x, tank_coordinate_y))
 
     keys = pygame.key.get_pressed()
-
+    #bullet movement trajectory
     if bullets:
-        for bul_in, el in enumerate(bullets):
-            el.x = -12
-            el.y = 0
+        for bul_ind, el in enumerate(bullets):
+            el.x += 12
+            el.y += 12
+            screen.blit(bullet_image, (el.x, el.y))
 
-    if keys[pygame.K_w] and tank_coord_y > 0:
-        tank_coord_y -= tank_speed
+    #Tank movement controll
+    if keys[pygame.K_w] and tank_coordinate_y > 0:
+        tank_coordinate_y -= tank_speed
         sounds[2].play()
         turn_1 = 0
-    elif keys[pygame.K_s] and tank_coord_y < 568:
-        tank_coord_y += tank_speed
+    elif keys[pygame.K_s] and tank_coordinate_y < 568:
+        tank_coordinate_y += tank_speed
         sounds[2].play()
         turn_1 = 180
-    elif keys[pygame.K_a] and tank_coord_x > 100:
-        tank_coord_x -= tank_speed
+    elif keys[pygame.K_a] and tank_coordinate_x > 100:
+        tank_coordinate_x -= tank_speed
         sounds[2].play()
         turn_1 = 90
-    elif keys[pygame.K_d] and tank_coord_x < 668:
-        tank_coord_x += tank_speed
+    elif keys[pygame.K_d] and tank_coordinate_x < 668:
+        tank_coordinate_x += tank_speed
         sounds[2].play()
         turn_1 = -90
     else:
@@ -91,12 +113,9 @@ while gameplay:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_j:
-                bullet.append[bullets]
+                bullets.append(bullet_image.get_rect(topleft=(tank_coordinate_x, tank_coordinate_y)))
                 pygame.mixer.Channel(0).play(sounds[7])
 
         if event.type == pygame.QUIT:
             gameplay = False
-            pygame.quit()
-    pygame.display.flip()
-    clock.tick(60)
-    screen.fill((10, 10, 10))
+pygame.quit()
